d483c3ba1d49dac05302dd1e76353cfedf363bff
jai <khambhaytajaydip@gmail.com>
2019-07-10 12:16:39 +0530

    Add files via upload

---
+++ ProfileImage.kt
@@ -0,0 +1,9 @@
+import android.graphics.Bitmap
+import java.util.*
+
+/**
+ * Created by JAI on 17,June,2019
+ */
+data class ProfileImage(val name:String,val image: Bitmap,val isCaptured :Boolean,val url:String = "") {
+
+}
